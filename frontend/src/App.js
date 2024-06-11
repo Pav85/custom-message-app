5015c0e4e10fd7499008518c3910addf775b86a1
Pawel <pawel.werbowy@gmail.com>
2024-06-11 09:15:52 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <div className="App"></div>;
 }
 
