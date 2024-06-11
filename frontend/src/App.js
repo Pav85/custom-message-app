f3c2745432909c2c6f7620157d4aa243cfd9306f
Pawel <pawel.werbowy@gmail.com>
2024-06-11 10:03:42 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,19 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState({});
+  const [customMessage, setCustomMessage] = useState("");
 
+  // hook to fetch data when the component mounts
   useEffect(() => {
     fetchData();
+  }, []); //
+
+  // hook to fetch message when the component mounts
+  useEffect(() => {
+    fetchMessage();
   }, []);
 
+  // function to fetch data from the backend
   const fetchData = async () => {
     try {
       const response = await axios.get("/api/data");
@@ -18,10 +26,22 @@ function App() {
     }
   };
 
+  // function to fetch message from the backend
+  const fetchMessage = async () => {
+    try {
+      const response = await axios.get("/api/message");
+      setCustomMessage(response.data.message);
+    } catch (error) {
+      console.error("Error fetching message: ", error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>{data.message || "Loading..."}</h1>
+        {/* custom message displayed in app */}
+        <h2>{customMessage || "Loading..."}</h2>
       </header>
     </div>
   );
